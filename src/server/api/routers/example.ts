706fe788995b86e7f744cb7313219b7a66c417e2
yoav sharon <yoav.sharon@unity3d.com>
2023-10-08 15:55:30 +0300

    change input to reporter fileds

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -4,10 +4,11 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .input(z.object({ location: z.optional(z.string()), name: z.optional(z.string()), id: z.optional(z.string()), picture: z.optional(z.string()), video: z.optional(z.string()), description: z.optional(z.string()), reporterName: z.string(), reporterPhone: z.string() }))
+    .query(({ input } : any) => {
+      const {location, name, id, picture, video, description, reporterName, reporterPhone} =input;
       return {
-        greeting: `Hello ${input.text}`,
+        greeting: `location: ${location}, name: ${name}, id: ${id}, picture: ${picture}, video: ${video}, description: ${description}, reporterName: ${reporterName}, reporterPhone: ${reporterPhone}`,
       };
     }),
 });
