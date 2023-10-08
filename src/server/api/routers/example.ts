2777ea2899151863eab575c8dd1157bb3912063b
yoav sharon <yoav.sharon@unity3d.com>
2023-10-08 15:56:48 +0300

    pretier

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -4,9 +4,29 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
-    .input(z.object({ location: z.optional(z.string()), name: z.optional(z.string()), id: z.optional(z.string()), picture: z.optional(z.string()), video: z.optional(z.string()), description: z.optional(z.string()), reporterName: z.string(), reporterPhone: z.string() }))
-    .query(({ input } : any) => {
-      const {location, name, id, picture, video, description, reporterName, reporterPhone} =input;
+    .input(
+      z.object({
+        location: z.optional(z.string()),
+        name: z.optional(z.string()),
+        id: z.optional(z.string()),
+        picture: z.optional(z.string()),
+        video: z.optional(z.string()),
+        description: z.optional(z.string()),
+        reporterName: z.string(),
+        reporterPhone: z.string(),
+      }),
+    )
+    .query(({ input }: any) => {
+      const {
+        location,
+        name,
+        id,
+        picture,
+        video,
+        description,
+        reporterName,
+        reporterPhone,
+      } = input;
       return {
         greeting: `location: ${location}, name: ${name}, id: ${id}, picture: ${picture}, video: ${video}, description: ${description}, reporterName: ${reporterName}, reporterPhone: ${reporterPhone}`,
       };
